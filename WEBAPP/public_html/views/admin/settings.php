e754b6bd03f46d0fa3dfbd87e28f5388b305ad6a
codemario90 <40020483+codemario90@users.noreply.github.com>
2018-06-08 18:05:35 +0300

    form changes

--- WEBAPP/public_html/views/admin/settings.php
+++ WEBAPP/public_html/views/admin/settings.php
@@ -16,71 +16,98 @@
                    <div class="col-md-12">
                     <section class="box ">
                         <header class="panel_header">
-                            <h2 class="title pull-left">Basic Elements</h2>
+                            <h2 class="title pull-left">Basic Information</h2>
                           
                         </header>
                         <div class="content-body">
                             <div class="row">
-                                <div class="col-md-12">
-                                    
-                                    
-                                    
-                                       <div class="form-group">
-                                     
-                                       
-                                        <div class="controls">
-                                            <input type="text" id="field-4" placeholder="Full name" class="form-control">
+                                <div class="col-md-4">
+                                <div class="form-group">
+                                     <div class="controls">
+                                            <input type="text" id="field-4" placeholder="First name" class="form-control">
                                         </div>
                                     </div>
+                                </div>
                                     
-                                    
-                                       <select class="form-control m-bot15">
-                                         <option>Select Gender</option>
-                                        <option>Male</option>
-                                        <option>Female</option>
-                                  
-                                    </select>
-                                    <br>
+                                <div class="col-md-4">
+                                  <div class="form-group">
+                                     <div class="controls">
+                                            <input type="text" id="field-4" placeholder="Middle name" class="form-control">
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                
+                                     <div class="col-md-4">
+                                      <div class="form-group">
+                                     <div class="controls">
+                                            <input type="text" id="field-4" placeholder="Last name" class="form-control">
+                                        </div>
+                                    </div>
+                                    </div>
+                            </div>
+                                   
+                                
+                                <div class="row">
+                                    <div class="col-md-4">
                                      <div class="form-group">
-                                     
-                                       
-                                        <div class="controls">
-                                            <span>Date</span>
+                                     <div class="controls">
+                                         <input name="gender" tabindex="5" type="radio" id="minimal-radio-1" class="icheck-minimal-red" checked>
+                                         <label class="iradio-label form-label" for="minimal-radio-1">Female</label>
+                                         
+                                         <input tabindex="5" name="gender" type="radio" id="minimal-radio-1" class="icheck-minimal-red" >
+                                         <label class="iradio-label form-label" for="minimal-radio-1">Male</label>
+                                        </div>
+                                    </div>
+                                    </div>
+                                    
+                                    <div class="col-md-4">
+                                     <div class="form-group">
+                                     <div class="controls">
                                             <input type="date" id="field-4" placeholder="" class="form-control">
                                         </div>
                                     </div>
-                               
+                                    </div>
+                                    
+                                    <div class="col-md-4">
                                      <div class="form-group">
-                                       
-                                     
-                                        <div class="controls">
+                                     <div class="controls">
+                                            <input type="text" id="field-4" placeholder="Phone number" class="form-control">
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+                                    
+                                <div class="row">
+                                     <div class="col-md-4">
+                                     <div class="form-group">
+                                     <div class="controls">
                                             <input type="text" id="field-4" placeholder="Email" class="form-control">
                                         </div>
                                     </div>
-                                     <div class="form-group">
-                                       
-                                        
-                                        <div class="controls">
-                                            <input type="text" id="field-4" placeholder="Phone" class="form-control">
-                                        </div>
                                     </div>
                                     
-
-                              
-
-                               
-                                      <select class="form-control m-bot15">
-                                         <option>Nationality</option>
-                                        <option>Kenya</option>
-                                        <option>Uganda</option>
-                                       <option>Tanzania</option>
-                                    </select>
-                                    <br>
                                     
-                                   
-                    
-                                 
-
+                                     <div class="col-md-4">
+                                     <div class="form-group">
+                                     <div class="controls">
+                                            <input type="text" id="field-4" placeholder="Phone Number" class="form-control">
+                                        </div>
+                                    </div>
+                                    </div>
+                              
+                                    <div class="col-md-4">
+                                     <div class="form-group">
+                                     <div class="controls">
+                                            <input type="text" id="field-4" placeholder="Occupation" class="form-control">
+                                        </div>
+                                    </div>
+                                    </div>
+                                </div>
+                                
+                                
+                                <div class="row">
+                                    <div class="col-md-6">
                                     <div class="form-group">
                                         <label class="form-label" for="field-6">Enter full physical address.</label>
                                        
@@ -88,6 +115,9 @@
                                             <textarea class="form-control" cols="5" id="field-6"></textarea>
                                         </div>
                                     </div>
+                                    </div>
+                                    
+                                </div>  
 
                                 <button type="submit" class="btn btn-primary">Submit</button>
                                 </div>
