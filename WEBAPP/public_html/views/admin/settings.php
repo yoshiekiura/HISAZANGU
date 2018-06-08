d4b71be855c3180ae50c9b94595669ad13629ed6
codemario90 <40020483+codemario90@users.noreply.github.com>
2018-06-08 17:15:30 +0300

    add form

--- WEBAPP/public_html/views/admin/settings.php
+++ WEBAPP/public_html/views/admin/settings.php
@@ -11,8 +11,93 @@
                             <h1 class="title">SETTINGS</h1>
                             <!-- PAGE HEADING TAG - END -->
                         </div>
+                         </div>
+                    
+                   <div class="col-md-12">
+                    <section class="box ">
+                        <header class="panel_header">
+                            <h2 class="title pull-left">Basic Elements</h2>
+                          
+                        </header>
+                        <div class="content-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    
+                                    
+                                    
+                                       <div class="form-group">
+                                     
+                                       
+                                        <div class="controls">
+                                            <input type="text" id="field-4" placeholder="Full name" class="form-control">
+                                        </div>
+                                    </div>
+                                    
+                                    
+                                       <select class="form-control m-bot15">
+                                         <option>Select Gender</option>
+                                        <option>Male</option>
+                                        <option>Female</option>
+                                  
+                                    </select>
+                                    <br>
+                                     <div class="form-group">
+                                     
+                                       
+                                        <div class="controls">
+                                            <span>Date</span>
+                                            <input type="date" id="field-4" placeholder="" class="form-control">
+                                        </div>
+                                    </div>
+                               
+                                     <div class="form-group">
+                                       
+                                     
+                                        <div class="controls">
+                                            <input type="text" id="field-4" placeholder="Email" class="form-control">
+                                        </div>
+                                    </div>
+                                     <div class="form-group">
+                                       
+                                        
+                                        <div class="controls">
+                                            <input type="text" id="field-4" placeholder="Phone" class="form-control">
+                                        </div>
+                                    </div>
+                                    
 
-                    </div>
+                              
+
+                               
+                                      <select class="form-control m-bot15">
+                                         <option>Nationality</option>
+                                        <option>Kenya</option>
+                                        <option>Uganda</option>
+                                       <option>Tanzania</option>
+                                    </select>
+                                    <br>
+                                    
+                                   
+                    
+                                 
+
+                                    <div class="form-group">
+                                        <label class="form-label" for="field-6">Enter full physical address.</label>
+                                       
+                                        <div class="controls">
+                                            <textarea class="form-control" cols="5" id="field-6"></textarea>
+                                        </div>
+                                    </div>
+
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </section>
+                </div>
+                        
+                   
                 </div>
           
 
